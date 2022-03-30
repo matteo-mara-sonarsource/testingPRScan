0da9cd303c22425790091b61df688599c8a37e1d
Matteo Mara <matteo.mara@sonarsource.com>
2022-03-30 21:31:24 +0200

    second commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -14,17 +14,10 @@ public class Main {
   @Deprecated(since = "1.0")
   private static final String DEPRECATED_STRING_2 = "deprecatedString2";
 
-  /**
-   * @deprecated (when, why, refactoring advice...)
-   */
-  @Deprecated(since = "1.0")
-  private static final String DEPRECATED_STRING_3 = "deprecatedString3";
-
   public static void main(String[] args) {
 
     if (DEPRECATED_STRING_1.equals("deprecatedString1")
-      && DEPRECATED_STRING_2.equals("deprecatedString2")
-      && DEPRECATED_STRING_3.equals("deprecatedString3")) {
+      && DEPRECATED_STRING_2.equals("deprecatedString2")) {
       System.out.println("All Strings are deprecated!");
     }
 
