90742c95a9aac9fdb20a5f0be73998428eef4eea
Matteo Mara <matteo.mara@sonarsource.com>
2022-03-30 17:45:25 +0200

    fifth commit with the same issues

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -17,7 +17,7 @@ public class Main {
   public static void main(String[] args) {
 
     if (DEPRECATED_STRING_1.equals("deprecatedString1")
-      && DEPRECATED_STRING_2.equals("deprecatedString2")) {
+      && DEPRECATED_STRING_2.equals("deprecatedString2") && true) {
 
       for (int i = 0; i <10 ; i++) {
         printMessage("message n^" + i);
